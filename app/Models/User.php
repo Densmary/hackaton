ff01eef5672e13917317bad4ec32809076031267
dvg2311a <david.gonzalez5@tecnacional.edu.ni>
2023-08-31 09:42:50 -0600

    Bueno

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'password'=> 'required',
         'age'=> 'required',
         //HI
-        
+        //ok
      ];
      protected $perPage= 20;
     protected $fillable = [
