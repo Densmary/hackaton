8841918426a2175b721a3190508f0f6e4169f954
Bilhan <latinobilhan@gmail.com>
2023-09-04 10:29:50 -0600

    time to text index function dtb, then layout then view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,7 @@ class UserController extends Controller
 
         return view('user.index', compact('users'))
             ->with('i', (request()->input('page', 1) - 1) * $users->perPage());
+            
     }
 
 }
