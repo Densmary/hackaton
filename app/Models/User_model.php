dd33526aa0faff2c9ecd35b591afdb77c4cf97a0
dvg2311a <david.gonzalez5@tecnacional.edu.ni>
2023-09-04 08:53:22 -0600

    Cambios en el modelo User. Creación de User de forma manual.

--- app/Models/User_model.php
+++ app/Models/User_model.php
@@ -8,17 +8,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User_model extends Model
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
+   /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-
-
 
      static $rules =
      [
