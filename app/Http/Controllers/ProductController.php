701f2fe7af59cac91029c5690160e7c27f6f92ea
dvg2311a <134893362+dvg2311a@users.noreply.github.com>
2023-09-05 09:17:13 -0600

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+<<<<<<< Updated upstream
     //
+=======
+    /***********************
+    public function store (Request $request)
+    {
+        $request->validate
+        ([
+            'photo'=> 'required│image│max:5120', 5 megabytes
+        ]);
+    }   
+    *********************** */
+>>>>>>> Stashed changes
 }
