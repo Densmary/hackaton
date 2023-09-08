d28679d9a1dcc10b66b1b7eca5ea7c180b8c92ff
dvg2311a <134893362+dvg2311a@users.noreply.github.com>
2023-09-08 09:50:13 -0600

    Vista arreglada , COntrolador y modelo listos.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\User_model;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Foundation\Auth\User as AuthUser;
 
 class UserController extends Controller {
     //
@@ -18,7 +18,7 @@ class UserController extends Controller {
     public function index()
     {
         /** change this declaration to the name of the class user model */
-        $users = User_model::paginate();
+        $users = AuthUser::paginate();
 
         return view('user.index', compact('users'))
             ->with('i', (request()->input('page', 1) - 1) * $users->perPage());
