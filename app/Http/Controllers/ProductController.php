6db8c07b5f433235b472aaaaaf74bdbcf3f1f129
dvg2311a <david.gonzalez5@tecnacional.edu.ni>
2023-09-12 19:07:22 +0200

    Cambios minimos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,19 +2,71 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Product_model;
 use Illuminate\Http\Request;
+
+
+
 
 class ProductController extends Controller
 {
-    /* 
-    **********************
-    public function store (Request $request)
+    /**
+     * Display a listing of the resource.
+     *@return \iluminate\Http\Response
+     */
+    public function index()
     {
-        $request->validate
-        ([
-            'photo'=> 'required│image│max:5120', 5 megabytes
-        ]);
-    }   
-    *********************** 
-    */
+        $productregister = Product_model::paginate();
+        return view ('blade.blade', compact('products'))
+        -> with('i' (request()->input('page' 1) -1) * $productregister->perPage());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
